de290eeeea19c528342942dc6b66214a36c3843c
Juan Pablo Mato Hernandez <juanpablomato89@gmail.com>
2024-03-02 13:00:47 -0500

    input and output

--- src/app/components/empleado-list/count-empleados/count-empleados.component.ts
+++ src/app/components/empleado-list/count-empleados/count-empleados.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-count-empleados',
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./count-empleados.component.css']
 })
 export class CountEmpleadosComponent implements OnInit {
+  radioButonSeleccionado = 'Todos';
+  @Input() todos: number = 0;
+  @Input() femenino: number = 0;
+  @Input() masculino: number = 0;
+  @Output() eventRadioButtonEmit = new EventEmitter<string>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  chageStateRadioButon() {
+    this.eventRadioButtonEmit.emit(this.radioButonSeleccionado);
+
   }
 
 }
--- src/app/components/empleado-list/empleado-list.component.ts
+++ src/app/components/empleado-list/empleado-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { empleado } from 'src/app/models/Empleado';
 
 @Component({
   selector: 'app-empleado-list',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./empleado-list.component.css']
 })
 export class EmpleadoListComponent implements OnInit {
+  listEmpleados: empleado[] = [
+    { legajo: 1, nombre: 'Juan', apellido: 'Perez', sexo: 'Masculino', salario: 25000 },
+    { legajo: 2, nombre: 'Marcos', apellido: 'Gozalez', sexo: 'Masculino', salario: 65000 },
+    { legajo: 3, nombre: 'Marta', apellido: 'Garcia', sexo: 'Femenino', salario: 75000 },
+    { legajo: 4, nombre: 'Ignacio', apellido: 'Cortes', sexo: 'Masculino', salario: 55000 },
+    { legajo: 5, nombre: 'Maria', apellido: 'Navarro', sexo: 'Femenino', salario: 80000 },
+  ]
+
+  radioButonSeleccionado = 'Todos';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  obtenerTotalEmpleados(): number {
+    return this.listEmpleados.length;
+  }
+
+  obtenerTotalFemeninos(): number {
+    return this.listEmpleados.filter(list => list.sexo === 'Femenino').length;
+  }
+
+  obtenerTotalMasculinos(): number {
+    return this.listEmpleados.filter(list => list.sexo === 'Masculino').length;
+  }
+
+  radioButonSeleccionadoChange(radioSeleccionado: string) {
+    this.radioButonSeleccionado = radioSeleccionado;
   }
 
 }
